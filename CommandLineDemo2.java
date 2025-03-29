d0afa48c341ed276957625e463b98efd48efbc57
Dhanush2451 <164873180+Dhanush2451@users.noreply.github.com>
2025-03-29 12:44:55 +0530

    Add files via upload

---
+++ CommandLineDemo2.java
@@ -0,0 +1,9 @@
+class CommandLineDemo2
+{
+public static void main(String args[])
+{
+System.out.println("Name :"+args[0]);
+System.out.println("section :"+args[1]);
+System.out.println("Branch :"+args[2]);
+}
+}
