2e1f05b5e1183e3c835654ffc9c9a47e6bdb723e
Dhanush2451 <164873180+Dhanush2451@users.noreply.github.com>
2025-03-29 12:02:24 +0530

    Add files via upload

---
+++ collegeDemo.java
@@ -0,0 +1,10 @@
+class collegeDemo
+{
+public static void main(String args[])
+{
+System.out.println("Name : Karnati Dhanush Reddy");
+System.out.println("College :MVSR");
+System.out.println("Roll No :2451-22-737-002");
+System.out.println("Branch :IT");
+}
+}
